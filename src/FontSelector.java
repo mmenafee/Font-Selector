5101797fe7fed7ac177087fd363a0d942cfd217b
Miles <mmenafee@deerfield.edu>
2016-01-26 11:50:25 -0500

    add gui

--- src/FontSelector.java
+++ src/FontSelector.java
@@ -1,11 +1,58 @@
 import javax.swing.*; 
 import java.awt.*; 
 import java.awt.event.*; 
-import javax.swing.JApplet;
-import javax.swing.JTextArea;
 
-public class FontSelector extends JApplet {
-	public void init(){
+
+public class FontSelector { 
+	
+	private JFrame f;
+	private JPanel p;
+	private JButton b1;
+	private JLabel lab;
+	
+	//constructor
+	public FontSelector(){
+		
+		gui();
+	}
+	
+	public void gui(){
+		f = new JFrame("Font Selector");
+		f.setVisible(true);
+		f.setSize(600,400);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		p = new JPanel(new GridBagLayout());
+		p.setBackground(Color.GRAY);
+		
+		b1 = new JButton("Font");
+		lab = new JLabel("This is test");
+		b1.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent e){
+				JOptionPane.showMessageDialog(null, "Action Listener Working");
+			}
+		});
+		
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.insets = new Insets(10,10,10,10);
+		c.gridx = 0;
+		c.gridy = 1;
+	
+		p.add(b1,c);
+		c.gridx = 0;
+		c.gridy = 2;
+		p.add(lab,c);
+		
+		f.add(p);
+	}
+	
+	public static void main(String[] args){
+		
+		new FontSelector();
 		
 	}
+	
 }
+
