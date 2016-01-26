0de88b22c8f256b402c4b655b26f1304f8df9305
Miles <mmenafee@deerfield.edu>
2016-01-26 07:45:35 -0500

    add java class

---
+++ src/FontSelector.java
@@ -0,0 +1,11 @@
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.event.*; 
+import javax.swing.JApplet;
+import javax.swing.JTextArea;
+
+public class FontSelector extends JApplet {
+	public void init(){
+		
+	}
+}
