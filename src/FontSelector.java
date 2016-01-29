804eab17f0980b78ba5da90147764834c52afbe6
cfreire11 <cfreire18@deerfield.edu>
2016-01-29 10:37:34 -0500

    Update FontSelector.java

--- src/FontSelector.java
+++ src/FontSelector.java
@@ -58,7 +58,18 @@ public class FontSelector {
 		lab = new JLabel("The quick brown fox jumped over the lazy dog’s back.");
 		lab.setFont(new Font("Times New Roman", Font.PLAIN, 15));
 		/** 
- 		*System.out.println("The quick brown fox jumped over the lazy dog’s back\nPack my box with five dozen liquor jugs.\nJackdaws love my big sphinx of quartz.\nMr. Jock, TV quiz PhD, bags few lynx.\nabcdefghijklmnopqrstuvwxyz\nABCDEFGHIJKLMNOPQRSTUVWXYZ\n01234567890\n€†™´¸¢©¤°÷½¼¾>¡¿«‘’<¯µ ·¬ªº¶±£\"»®§­¹²³ß×™¨¥\nÀÁÂÃÄÅÆÇÈÉ ÊËÌÍÎÏÐÑÒÓÔ ÕÖØÙÚÛÜÝÞÿ\nàáâãäåæçèé êëìíîïðñòóô õöøùúûüýþÿ\n!\"#$%&'()*+,-./:;<=>?@[\\^_z{|}~\nuvw wW gq9 2z 5s il17|!j oO08 `\'\" ;:,. m nn rn {[()]}u");
+ 		*System.out.println("The quick brown fox jumped over the lazy dog’s back."
+ 		* + "\nPack my box with five dozen liquor jugs."
+ 		* + "\nJackdaws love my big sphinx of quartz.
+ 		* + "\nMr. Jock, TV quiz PhD, bags few lynx."
+ 		* + "\nabcdefghijklmnopqrstuvwxyz"
+ 		* + "\nABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ 		* + "\n01234567890"
+ 		* + "\n€†™´¸¢©¤°÷½¼¾>¡¿«‘’<¯µ ·¬ªº¶±£\"»®§­¹²³ß×™¨¥"
+ 		* + "\nÀÁÂÃÄÅÆÇÈÉ ÊËÌÍÎÏÐÑÒÓÔ ÕÖØÙÚÛÜÝÞÿ"
+ 		* + "\nàáâãäåæçèé êëìíîïðñòóô õöøùúûüýþÿ"
+ 		* + "\n!\"#$%&'()*+,-./:;<=>?@[\\^_z{|}~"
+ 		* + "\nuvw wW gq9 2z 5s il17|!j oO08 `\'\" ;:,. m nn rn {[()]}u");
 		*/
 		
 		
