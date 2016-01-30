1dd1be5c3afce12cba96eb6ec0a579958e1a8fe2
mmenafee <mmenafee@deerfield.edu>
2016-01-30 09:28:13 -0500

    add change listeners to color and font slider

--- src/FontSelector.java
+++ src/FontSelector.java
@@ -1,7 +1,13 @@
-import javax.swing.*; 
+import javax.swing.*;
+import javax.swing.colorchooser.ColorSelectionModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*; 
 import java.awt.event.*; 
 import java.awt.GraphicsEnvironment;
+import java.awt.Font;
 
 
 public class FontSelector { 
@@ -12,92 +18,113 @@ public class FontSelector {
 	private JLabel lab;
 	
     GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    JColorChooser txtChoose = new JColorChooser();
+    String swag = 
+     		("The quick brown fox jumped over the lazy dog’s back."
+     		 + "\nPack my box with five dozen liquor jugs."
+     		 + "\nJackdaws love my big sphinx of quartz."
+     		 + "\nMr. Jock, TV quiz PhD, bags few lynx."
+     		 + "\nabcdefghijklmnopqrstuvwxyz"
+     		 + "\nABCDEFGHIJKLMNOPQRSTUVWXYZ"
+     		 + "\n01234567890"
+     		 + "\n€†™´¸¢©¤°÷½¼¾>¡¿«‘’<¯µ ·¬ªº¶±£\"»®§­¹²³ß×™¨¥"
+     		 + "\nÀÁÂÃÄÅÆÇÈÉ ÊËÌÍÎÏÐÑÒÓÔ ÕÖØÙÚÛÜÝÞÿ"
+     		 + "\nàáâãäåæçèé êëìíîïðñòóô õöøùúûüýþÿ"
+     		 + "\n!\"#$%&'()*+,-./:;<=>?@[\\^_z{|}~"
+     		 + "\nuvw wW gq9 2z 5s il17|!j oO08 `\'\" ;:,. m nn rn {[()]}u");
+    		
+    JTextArea text = new JTextArea(swag);
+    JScrollPane txtScroll = new JScrollPane(text);
     String[] names = ge.getAvailableFontFamilyNames();
-	JComboBox fonts = new JComboBox(names);
-	String[] iBold ={"ITALIC","BOLD","PLAIN"};
-	JComboBox styles = new JComboBox(iBold);
-	String[] num = {"12","14","16","18","20","22","24"};
-	JComboBox size = new JComboBox(num);
-	String[] exam = {"The quick brown fox jumped over the lazy dog’s back.",
-			"Pack my box with five dozen liquor jugs.","Jackdaws love my big sphinx of quartz.",
-			"Mr. Jock, TV quiz PhD, bags few lynx.","abcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-			"01234567890","€†™´¸¢©¤°÷½¼¾>¡¿«‘’<¯µ ·¬ªº¶±£»®§­¹²³ß×™¨¥","ÀÁÂÃÄÅÆÇÈÉ ÊËÌÍÎÏÐÑÒÓÔ ÕÖØÙÚÛÜÝÞÿ",
-			"àáâãäåæçèé êëìíîïðñòóô õöøùúûüýþÿ","!#$%&'()*+,-./:;<=>?@[^_z{|}~",
-			"uvw wW gq9 2z 5s il17|!j oO08 `' ;:,. m nn rn {[()]}u"
-	};
-	JComboBox examples = new JComboBox(exam);
-
+	JList fonts = new JList(names);
+	JSlider slide = new JSlider(4,96,12);
+	JScrollPane scrollPane = new JScrollPane(fonts);
+	
+	
+	
 	//constructor
 	public FontSelector(){
 		gui();
+		fonts.setCellRenderer(new FontCellRenderer());
 	}
 	
+	class FontCellRenderer extends DefaultListCellRenderer {
+
+	    public Component getListCellRendererComponent(
+	        JList list,
+	        Object value,
+	        int index,
+	        boolean isSelected,
+	        boolean cellHasFocus) {
+	        JLabel label = (JLabel)super.getListCellRendererComponent(
+	            list,value,index,isSelected,cellHasFocus);
+	        Font font = new Font((String)value, Font.PLAIN, 20);
+	        label.setFont(font);
+	        return label;
+	    }
+	}
+	
+
 	public void gui(){
 		f = new JFrame("Font Selector");
 		f.setVisible(true);
 		f.setSize(1000,1000);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		slide.setMajorTickSpacing(10);
+		slide.setMinorTickSpacing(1);
+		slide.setPaintTicks(true);
+		slide.setPaintLabels(true);
 		p = new JPanel(new GridBagLayout());
 		p.setLayout(new GridLayout(3,3,10,10));
-		p.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-		p.setBackground(Color.GRAY);
+		p.setBackground(Color.LIGHT_GRAY);
 		
-		b1 = new JButton("The quick brown fox jumped over the lazy dog’s back/n"
-			+ "Pack my box with five dozen liquor jugs./n"
-			+ "Jackdaws love my big sphinx of quartz./n"
-			+ "Mr. Jock, TV quiz PhD, bags few lynx./n"
-			+ "abcdefghijklmnopqrstuvwxyz/n"
-			+ "ABCDEFGHIJKLMNOPQRSTUVWXYZ/n"
-			+ "01234567890/n"
-			+ "€†™´¸¢©¤°÷½¼¾>¡¿«‘’<¯µ ·¬ªº¶±£»®§­¹²³ß×™¨¥/n"
-			+ "ÀÁÂÃÄÅÆÇÈÉ ÊËÌÍÎÏÐÑÒÓÔ ÕÖØÙÚÛÜÝÞÿ/n"
-			+ "àáâãäåæçèé êëìíîïðñòóô õöøùúûüýþÿ/n"
-			+ "!#$%&'()*+,-./:;<=>?@[^_z{|}~/n"
-			+ "uvw wW gq9 2z 5s il17|!j oO08 `' ;:,. m nn rn {[()]}u");
+		b1 = new JButton("Click Here to See Font");
 		lab = new JLabel("The quick brown fox jumped over the lazy dog’s back.");
 		lab.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-		/** 
- 		*System.out.println("The quick brown fox jumped over the lazy dog’s back."
- 		* + "\nPack my box with five dozen liquor jugs."
- 		* + "\nJackdaws love my big sphinx of quartz.
- 		* + "\nMr. Jock, TV quiz PhD, bags few lynx."
- 		* + "\nabcdefghijklmnopqrstuvwxyz"
- 		* + "\nABCDEFGHIJKLMNOPQRSTUVWXYZ"
- 		* + "\n01234567890"
- 		* + "\n€†™´¸¢©¤°÷½¼¾>¡¿«‘’<¯µ ·¬ªº¶±£\"»®§­¹²³ß×™¨¥"
- 		* + "\nÀÁÂÃÄÅÆÇÈÉ ÊËÌÍÎÏÐÑÒÓÔ ÕÖØÙÚÛÜÝÞÿ"
- 		* + "\nàáâãäåæçèé êëìíîïðñòóô õöøùúûüýþÿ"
- 		* + "\n!\"#$%&'()*+,-./:;<=>?@[\\^_z{|}~"
- 		* + "\nuvw wW gq9 2z 5s il17|!j oO08 `\'\" ;:,. m nn rn {[()]}u");
-		*/
 		
-		
-		b1.addActionListener(new ActionListener(){
-		
-			public void actionPerformed(ActionEvent e){
-				String fo = fonts.getSelectedItem().toString();
-				String st = styles.getSelectedItem().toString();
-				String ex = examples.getSelectedItem().toString();
-				int si = Integer.valueOf((String)size.getSelectedItem());
-				lab.setFont(new Font(fo, Font.PLAIN, si));
-				lab.setText(ex);
-			}
-		});
+	    fonts.addListSelectionListener(new ListSelectionListener() {
 
+			public void valueChanged(ListSelectionEvent e) // value in list changes so example text does
+			{ 
+				int size = slide.getValue();
+				String fo = fonts.getSelectedValue().toString();
+				text.setFont(new Font(fo, Font.PLAIN, size));
+			}
+	      });
 		
-		p.add(fonts);
-		p.add(styles);
-		p.add(size);
+	    ColorSelectionModel model = txtChoose.getSelectionModel();
+	    //every color change
+	    ChangeListener changeListener = new ChangeListener() { 
+	      public void stateChanged(ChangeEvent changeEvent) {
+	        Color newForegroundColor = txtChoose.getColor();
+	        text.setForeground(newForegroundColor);
+	      }
+	    };
+	    model.addChangeListener(changeListener);
+	    //slider text size
+	    
+	    slide.addChangeListener(new ChangeListener() {
+	        public void stateChanged(ChangeEvent evt) {
+	          JSlider slider = (JSlider) evt.getSource();
+	          if (slider.getValueIsAdjusting()) {
+	            float value = slider.getValue();
+	            
+	            text.setFont(text.getFont().deriveFont(value));
+	          }
+	        }
+	      });
+		
+		p.add(scrollPane);
+		p.add(slide);
+		p.add(txtChoose);
+		p.add(txtScroll);
 		p.add(b1);
-		p.add(examples);
-		p.add(lab);
 		f.add(p);
 	}
 	
 	public static void main(String[] args){
 		new FontSelector();
-		
+			
 	}
 	
 }
